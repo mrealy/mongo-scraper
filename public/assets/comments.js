9df628a1233da85627f8c5cc7acfc992f542fcde
Michael Realy <mjrealy@gmail.com>
2018-01-22 21:50:50 -0600

    Changed jQuery to render comments to div within handlebars list item after selected button.  Also renders input UI

--- public/assets/comments.js
+++ public/assets/comments.js
@@ -1,32 +1,30 @@
 $(document).ready(function() {
-
-    // $("#comment").hide();
-    var selId;
-
+    // 
     $(".comment").on("click", function() {
 
-        //$("#comment").css("visibility", "visible");
+        $("#test").remove();
         $("#comment").empty();
 
         // pull button value (id) and store into variable
         var thisId = $(this).val();
+        var currentButton = $(this);
+        //console.log("thisId from comments.js jQuery file is ", thisId);
 
         $.ajax({
             method: "GET",
             url: "/comments/" + thisId
         }).done(function(data) {
-            console.log(data);
+            console.log("Article comments after GET: ", data.comments);
             
-            $("#comment").append("<h4>" + data.title + "</h4>");
-            $("#comment").append("<input id='commentTitle' name='title'>");
-            $("#comment").append("<textarea id='commentBody' name='body'></textarea>");
-            $("#comment").append("<button class='btn-submit btn-sm' data-id='" + data._id + "' id='commentSubmit'>Save comment</button>");
-            
-            if (data.comment) {
-                $("#commentTitle").attr("placeholder", data.comment.title + " (current title)");
-                $("#commentBody").attr("placeholder", data.comment.body + " (current comment)");
-            }
-        })
+            // Generates the comment input UI for article            
+            displayComments(data.comments, currentButton);
+            $("#test").append("<h4>" + data.title + "</h4>");
+            $("#test").append("<input id='commentTitle' name='title'>");
+            $("#test").append("<textarea id='commentBody' name='body'></textarea>");
+            $("#test").append("<button class='btn-submit btn-sm' data-id='" + data._id + "' id='commentSubmit'>Save comment</button>");
+
+        });
+        
     });
 
     // $("#commentSubmit").on("click", function() {
@@ -42,11 +40,32 @@ $(document).ready(function() {
                 body: $("#commentBody").val()
             }
         }).done(function(data) {
-            console.log(data);
+            console.log("articles comments after POST: ", data.comments);
             $("#comments").empty();
         });
         $("#commentTitle").val("");
         $("#commentBody").val("");
     });
+
+    function displayComments(comments, currentButton) {
+        console.log("in displayComments ", currentButton);
+        currentButton.after("<div id='test'></div>");
+        for (var i = 0; i <= comments.length; i++) {
+            commentID = comments[i];
+            $.ajax({
+                method: "GET",
+                url: "/comment/" + commentID,
+            }).done(function(data) {
+                console.log("Comment data in displayComments " + i + " is", data);
+                $("#test").append("<div id='comment-container-"+i+"' class='comment-container'></div>");
+                $("#comment-container-"+i).append("<p> Comment title is " + data.title + "</p>");
+                $("#comment-container-"+i).append("<p> Comment body is " + data.body + "</p>");              
+            });
+        } 
+        // $.ajax({
+        //     method: "GET",
+        //     url: "/co"
+        // })
+    }
 
 });
