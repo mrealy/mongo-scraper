b35cda8f7e56ccb5b86d713377cd7e412c96d02c
Michael Realy <mjrealy@gmail.com>
2018-01-24 22:23:22 -0600

    Added AJAX call and jQuery appends to pull comments associated with the clicked article and render them to the page.

--- public/assets/comments.js
+++ public/assets/comments.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     // 
     $(".comment").on("click", function() {
 
-        $("#test").remove();
+        $("#comments").remove();
         $("#comment").empty();
 
         // pull button value (id) and store into variable
@@ -17,11 +17,7 @@ $(document).ready(function() {
             console.log("Article comments after GET: ", data.comments);
             
             // Generates the comment input UI for article            
-            displayComments(data.comments, currentButton);
-            $("#test").append("<h4>" + data.title + "</h4>");
-            $("#test").append("<input id='commentTitle' name='title'>");
-            $("#test").append("<textarea id='commentBody' name='body'></textarea>");
-            $("#test").append("<button class='btn-submit btn-sm' data-id='" + data._id + "' id='commentSubmit'>Save comment</button>");
+            displayComments(data, currentButton);
 
         });
         
@@ -36,36 +32,51 @@ $(document).ready(function() {
             method: "POST",
             url: "/comments/" + thisId,
             data: {
-                title: $("#commentTitle").val(),
-                body: $("#commentBody").val()
+                title: $("#comment-user").val(),
+                body: $("#comment-body").val()
             }
         }).done(function(data) {
             console.log("articles comments after POST: ", data.comments);
             $("#comments").empty();
         });
-        $("#commentTitle").val("");
-        $("#commentBody").val("");
+        $("#comment-user").val("");
+        $("#comment-body").val("");
     });
 
-    function displayComments(comments, currentButton) {
+    function displayComments(data, currentButton) {
         console.log("in displayComments ", currentButton);
-        currentButton.after("<div id='test'></div>");
+        var comments = data.comments;
+        currentButton.after("<div id='comments'></div>");
+        $("#comments").append("<div id='comments-input'></div>");
+        $("#comments-input").append("<h4> Article comments </h4>");
         for (var i = 0; i <= comments.length; i++) {
-            commentID = comments[i];
-            $.ajax({
-                method: "GET",
-                url: "/comment/" + commentID,
-            }).done(function(data) {
-                console.log("Comment data in displayComments " + i + " is", data);
-                $("#test").append("<div id='comment-container-"+i+"' class='comment-container'></div>");
-                $("#comment-container-"+i).append("<p> Comment title is " + data.title + "</p>");
-                $("#comment-container-"+i).append("<p> Comment body is " + data.body + "</p>");              
-            });
-        } 
-        // $.ajax({
-        //     method: "GET",
-        //     url: "/co"
-        // })
+            (function(i) {
+                if (i < comments.length) {
+                    
+                    commentID = comments[i];
+                    $.ajax({
+                        method: "GET",
+                        url: "/comment/" + commentID,
+                    }).done(function(data) {
+                        //console.log("Comment data in displayComments " + i + " is", data);
+                        $("#comments").append("<div id='comment-container-"+i+"' class='comment-container'></div>");                                    
+                        $("#comment-container-"+i).append("<p> Comment title is " + data.title + "</p>");
+                        $("#comment-container-"+i).append("<p> Comment body is " + data.body + "</p>");                 
+                    });
+                }
+                if (i === comments.length) {
+                    renderCommentInput();
+                }
+            })(i);
+        }
+        function renderCommentInput() { 
+            $("#comments-input").append("<div class='form-group'></div>")
+            $(".form-group").append("<input id='comment-user' name='title' type='text' class='pull-right'>");            
+            $(".form-group").append("<label for='comment-user' class='pull-right'>User:</label>");            
+            $(".form-group").append("<label for='comment-body'>Comment:</label>");
+            $(".form-group").append("<textarea id='comment-body' name='body' class='form-control' rows='2'></textarea>");
+            $("#comments-input").append("<button class='btn-submit btn-sm btn-success' data-id='" + data._id + "' id='commentSubmit'>Save comment</button>");
+        }
     }
 
 });
